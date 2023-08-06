e5129e8d8c43a8472d4ffea4502cf5c19ca08506
Rajiv620019 <rajiv620019@gmail.com>
2023-08-06 12:11:27 +0530

    Setup token name and symbol

--- src/Lamp.sol
+++ src/Lamp.sol
@@ -2,8 +2,10 @@
 pragma solidity 0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Lamp {
+contract Lamp is ERC20 {
     
-    
-
+    // Constructor to pass name and symbol
+    constructor (uint256 initialSupply) ERC20("Lamp Token", "LMP") {
+        _mint(msg.sender, initialSupply);
+    }
 }
